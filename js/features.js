3773ba33a176bd9e6f5c65db03e8c8008f31f40c
L10X17 <3352711455@qq.com>
2026-09-11 14:04:39 +0800

    Update features.js

--- js/features.js
+++ js/features.js
@@ -194,7 +194,7 @@
 
     function _createAudio() {
         if (_audio) return _audio;
-        _audio = new Audio(SRC);
+        _audio = new Audio("https://raw.githubusercontent.com/L10X17/-/main/dkb.m4a");
         _audio.loop   = true;
         _audio.volume = 0.01;
         _audio.preload = 'auto';
